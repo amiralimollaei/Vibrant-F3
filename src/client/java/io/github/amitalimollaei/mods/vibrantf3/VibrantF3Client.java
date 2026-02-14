61a6b7eaacb36e209ddf174161d8c55a4fc6ad90
AmirAli Mollaei <me.amiralimollaei@gmail.com>
2026-02-15 01:55:09 +0330

    added debug toggle keybinding

--- src/client/java/io/github/amitalimollaei/mods/vibrantf3/VibrantF3Client.java
+++ src/client/java/io/github/amitalimollaei/mods/vibrantf3/VibrantF3Client.java
@@ -1,16 +1,38 @@
 package io.github.amitalimollaei.mods.vibrantf3;
 
+import com.mojang.blaze3d.platform.InputConstants;
 import eu.midnightdust.lib.config.MidnightConfig;
 import io.github.amitalimollaei.mods.vibrantf3.storage.Config;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.KeyMapping;
+import net.minecraft.resources.Identifier;
+import org.lwjgl.glfw.GLFW;
 
 
 public class VibrantF3Client implements ClientModInitializer {
     public static final String MOD_ID = "vibrant_f3";
 
+    private static KeyMapping debugKeyBinding;
+    private static final KeyMapping.Category CATEGORY = KeyMapping.Category.register(Identifier.fromNamespaceAndPath(MOD_ID, "key.category"));
+
     @Override
     public void onInitializeClient() {
         MidnightConfig.init(MOD_ID, Config.class);
+        debugKeyBinding = new KeyMapping(
+                "vibrant_f3.key.debug",
+                InputConstants.Type.KEYSYM,
+                GLFW.GLFW_KEY_F7,
+                CATEGORY
+        );
+        KeyBindingHelper.registerKeyBinding(debugKeyBinding);
+
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            while (debugKeyBinding.consumeClick()) {
+                Config.debugEnabled = !Config.debugEnabled;
+            }
+        });
     }
 
     public static void saveConfig() {
